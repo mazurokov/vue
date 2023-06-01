d0dd842475c408583531362ebe0d4a79e0528590
oleksandrmazurok <mazurok0120@gmail.com>
2023-06-01 23:15:09 +0300

    initial

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,36 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
+    <header class="header">
+      <div class="container">
+        <nav class="nav">
+          <router-link to="/">Home</router-link>
+          <router-link to="/view-page">ViewPage</router-link>
+          <router-link to="/about">About</router-link>
+        </nav>
+      </div>
+    </header>
+    <!--<HomePage></HomePage>-->
     <router-view/>
+    <footer class="footer">
+      <div class="container">
+
+      </div>
+    </footer> 
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+/*
+// @ is an alias to /src
+import HomePage from '@/views/HomePage.vue.js'
 
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+export default {
+  name: 'HomePage',
+  components: {
+    HomePage,
   }
 }
-</style>
+*/
+</script>
+
+<style lang="sass" src="./assets/sass/style.sass"></style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,20 @@
 import Vue from 'vue'
+import HomePage from '../views/HomePage.vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import ViewPage from '../views/ViewPage.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'Home',
+    component: HomePage
+  },
+  {
+    path: '/view-page',
+    name: 'ViewPage',
+    component: ViewPage
   },
   {
     path: '/about',
@@ -16,7 +22,7 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component: () => import('../views/AboutView.vue')
   }
 ]
 
