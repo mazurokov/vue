45fb8347500d26e03be5e9071df98cc41fb3d92c
oleksandrmazurok <mazurok0120@gmail.com>
2023-06-23 16:19:48 +0300

    feat:(search)

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,22 @@
 <template>
   <div id="app">
-    <header class="header">
-      <div class="container">
-        <nav class="nav">
-          <router-link to="/">Home</router-link>
-          <router-link to="/view-page">ViewPage</router-link>
-          <router-link to="/about">About</router-link>
-        </nav>
-      </div>
-    </header>
-    <!--<HomePage></HomePage>-->
+    <HeaderTemplate/>
     <router-view/>
-    <footer class="footer">
-      <div class="container">
-
-      </div>
-    </footer> 
   </div>
 </template>
 
 <script>
-/*
+
 // @ is an alias to /src
-import HomePage from '@/views/HomePage.vue.js'
+import HeaderTemplate from '@/components/Header/Header.vue'
 
 export default {
   name: 'HomePage',
   components: {
-    HomePage,
+    HeaderTemplate,
   }
 }
-*/
+
 </script>
 
-<style lang="sass" src="./assets/sass/style.sass"></style>
+<style lang="sass" src="@/assets/sass/style.sass"></style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,16 @@
 import Vue from 'vue'
-import HomePage from '../views/HomePage.vue'
 import VueRouter from 'vue-router'
-import ViewPage from '../views/ViewPage.vue'
+
+import SearchPage from '../views/SearchPage/SearhcPage.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: HomePage
+    path: '/search',
+    name: 'SearchPage',
+    component: SearchPage
   },
-  {
-    path: '/view-page',
-    name: 'ViewPage',
-    component: ViewPage
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import('../views/AboutView.vue')
-  }
 ]
 
 const router = new VueRouter({
